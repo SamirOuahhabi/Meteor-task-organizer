d469b6b3d208745ef1fe3abc84904b6e73584c8f
Samir Ouahhabi <samir@ouahhabi.com>
2017-02-16 00:38:11 -0800

    Add pub/sub, user accounts, methods. v1 Basic functionality complete

--- imports/api/tasks.js
+++ imports/api/tasks.js
@@ -1,3 +1,52 @@
+import { Meteor } from 'meteor/meteor';
 import { Mongo } from 'meteor/mongo';
+import { check } from 'meteor/check';
  
 export const Tasks = new Mongo.Collection('tasks');
+
+if (Meteor.isServer) {
+  // This code only runs on the server
+  Meteor.publish('tasks', function tasksPublication() {
+    // TODO publish
+    return Tasks.find({userId: this.userId});
+  });
+}
+
+Meteor.methods({
+  'tasks.insert'(task) {
+    check(task.todo, String);
+    check(task.outcome, String);
+    check(task.desire, String);
+ 
+    // Make sure the user is logged in before inserting a task
+    if (! this.userId) {
+      throw new Meteor.Error('not-authorized');
+    }
+ 
+    Tasks.insert({
+      todo: task.todo,
+      outcome: task.outcome,
+      desire: task.desire,
+      userId: this.userId,
+      createdAt: new Date(), // current time
+    });
+
+  },
+  'tasks.remove'(taskId) {
+    check(taskId, String);
+    const task = Tasks.findOne(taskId);
+    if (task.userId !== this.userId) {
+      throw new Meteor.Error('not-authorized');
+    }
+    Tasks.remove(taskId);
+  },
+  'tasks.setChecked'(taskId, setChecked) {
+    check(taskId, String);
+    check(setChecked, Boolean);
+    const task = Tasks.findOne(taskId);
+    if (task.owner !== this.userId) {
+      throw new Meteor.Error('not-authorized');
+    }
+    Tasks.update(taskId, { $set: { checked: setChecked } });
+  },
+});
